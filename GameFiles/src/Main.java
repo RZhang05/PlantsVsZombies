01157bbd02cf1d01a4c9966bce02396e60b2b6e3
SlowestLoris <zhangraymond05@gmail.com>
2018-01-12 08:01:52 -0500

    Update Main.java

--- GameFiles/src/Main.java
+++ GameFiles/src/Main.java
@@ -75,7 +75,6 @@ public class Main {
 
 	//global variables
 	public static Timer globalTime = new Timer();
-	public static boolean gameLost = false;
 
 	//the plants currently in play
 	static ArrayList<Plant> shooterPlants = new ArrayList<Plant>();
@@ -85,6 +84,7 @@ public class Main {
 
 	//plants
 	public static int plantSelected = 0;
+	public static int index; //dead plant
 
 	//The Board
 	static Board b;
@@ -106,6 +106,7 @@ public class Main {
 
 	//Game End Flag
 	static boolean isRunning = true;
+	static int gameLost = 0; //did you lose the game (0 = still going, 1 = won, 2 = loss)
 
 	public static void main(String[] args) {
 
@@ -158,7 +159,7 @@ public class Main {
 				keepZombieSprites();
 			}
 		};
-		
+
 		TimerTask playZombieNoises = new TimerTask() {
 			@Override
 			public void run() {
@@ -261,7 +262,6 @@ public class Main {
 		globalTime.scheduleAtFixedRate(sunflowerAnim, (long)1000, (long)2500);
 		globalTime.scheduleAtFixedRate(wallnutChange, (long)1000, (long)400);
 		globalTime.scheduleAtFixedRate(animZombies, (long)20000, (long)2000);
-		globalTime.scheduleAtFixedRate(updateClock, (long)1000, (long)1000);
 		globalTime.schedule(playZombieNoises, (long)21000, (long)6500);
 
 
@@ -279,6 +279,10 @@ public class Main {
 				mainGame.repaint();
 			}
 			else {
+				if(gameTime == 0) {
+					globalTime.scheduleAtFixedRate(updateClock,(long)0, (long)1000);
+				}
+
 				StoredEnergy.setText("Stored Energy: " + sunCount);
 				b.setVisible(true);
 				startScreen.setVisible(false);
@@ -292,6 +296,26 @@ public class Main {
 				mainGame.add(button4);
 				mainGame.add(StoredEnergy);
 				mainGame.add(gameTimer);
+				
+				//end game
+				if(gameLost == 2) {
+					System.out.println("Running processes");
+
+					//end game screens
+					b.setVisible(false);
+					button1.setVisible(false);
+					button2.setVisible(false);
+					button3.setVisible(false);
+					button4.setVisible(false);
+					StoredEnergy.setVisible(false);
+					gameTimer.setVisible(false);
+					globalTime.cancel();
+					mainGame.repaint();
+
+					EndGame endScreen = new EndGame();
+					mainGame.add(endScreen);
+					endScreen.setVisible(true);
+				}
 
 				//if you click anywhere on the board
 				Coordinate grow = b.getClick();
@@ -354,7 +378,7 @@ public class Main {
 					}
 				}
 			}
-		}	
+		}
 	}
 
 	/**
@@ -366,114 +390,109 @@ public class Main {
 		//for each pea shooter
 		for(int i=0;i<plants.size();i++) {
 			Plant tempPlant = plants.get(i);
-			if(killPlant(tempPlant)) {
-				b.removePeg(tempPlant.originCol, tempPlant.originRow);
-				plants.remove(tempPlant);
-			} else {
-				//if a zombie is in the lane
-				boolean zombieInLane = false;
+			//if a zombie is in the lane
+			boolean zombieInLane = false;
 
-				int smallest = Integer.MAX_VALUE;
-				//the index of the closed zombie to the plant
-				int index = 0;
-				//create a default zombie
-				Zombie thisZomb = new Zombie(0,0);
-				//if zombie in lane
-				for(int j=0;j<zombies.size();j++) {
-					Zombie curZombie = zombies.get(j);
-					//check for comparison 
-					System.out.println("Comparing " + tempPlant.originRow + " to " + curZombie.getRow());
-					if(curZombie.getRow() == tempPlant.originRow) {
-						zombieInLane = true;
-						if(curZombie.getCol() < smallest) {
-							smallest = curZombie.getCol();
-							thisZomb = curZombie;
-						}
+			int smallest = Integer.MAX_VALUE;
+			//the index of the closed zombie to the plant
+			int index = 0;
+			//create a default zombie
+			Zombie thisZomb = new Zombie(0,0);
+			//if zombie in lane
+			for(int j=0;j<zombies.size();j++) {
+				Zombie curZombie = zombies.get(j);
+				//check for comparison 
+				System.out.println("Comparing " + tempPlant.originRow + " to " + curZombie.getRow());
+				if(curZombie.getRow() == tempPlant.originRow) {
+					zombieInLane = true;
+					if(curZombie.getCol() < smallest) {
+						smallest = curZombie.getCol();
+						thisZomb = curZombie;
 					}
 				}
-				if(zombieInLane) {
-					
-					//cooldown
-					if(tempPlant.lastShot != 0 && tempPlant.lastShot >= gameTime && tempPlant.getCol() <= tempPlant.originCol) {
-						System.out.println("Cooldown");
-						return; //1 second has passed?
+			}
+			if(zombieInLane) {
+
+				//cooldown
+				if(tempPlant.lastShot != 0 && tempPlant.lastShot >= gameTime && tempPlant.getCol() <= tempPlant.originCol) {
+					System.out.println("Cooldown");
+					return; //1 second has passed?
+				}
+
+				tempPlant.lastShot = gameTime;
+				System.out.println(tempPlant.lastShot + " compared to: " + gameTime);
+				// Testing for zombie in lane
+				// System.out.println("Shooting at Zombie");
+				if(tempPlant.type == 2) { //PEA SHOOTER
+					if(tempPlant.getCol() < 9) {
+						//animation
+						b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+						b.putPeg("peashooter", tempPlant.originRow, tempPlant.originCol);
+						b.putPeg("pea", tempPlant.getRow(),tempPlant.getCol() + 1);
+
+						//shooting noise
+						if(tempPlant.getCol() == tempPlant.originCol + 1) {
+							playPeaShoot();
+						}
+
+						tempPlant.column = tempPlant.column + 1;
+						if(tempPlant.getCol() == smallest) {
+							b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+
+							//reset the plant animation
+							tempPlant.row = tempPlant.originRow;
+							tempPlant.column = tempPlant.originCol;
+
+							thisZomb.hp -= 2;
+							playHitZombie();
+							if(thisZomb.hp <= 0) {
+								//testing for which zombie is dying
+								System.out.println(zombies.get(index).getRow());
+								zombies.remove(index);
+
+								b.removePeg(thisZomb.getRow(), thisZomb.getCol());
+								try{Thread.sleep(1);}catch(InterruptedException e){};
+							}
+						}
+					} else {
+						b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+						tempPlant.row = tempPlant.originRow;
+						tempPlant.column = tempPlant.originCol;
 					}
-					
-					tempPlant.lastShot = gameTime;
-					System.out.println(tempPlant.lastShot + " compared to: " + gameTime);
-					// Testing for zombie in lane
-					// System.out.println("Shooting at Zombie");
-					if(tempPlant.type == 2) { //PEA SHOOTER
-						if(tempPlant.getCol() < 9) {
-							//animation
+				} else { //DOUBLE PEA SHOOTER
+					if(tempPlant.getCol() < 9) {
+						//animation
+						b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+						b.putPeg("doublepea", tempPlant.originRow, tempPlant.originCol);
+						b.putPeg("pea", tempPlant.getRow(),tempPlant.getCol() + 1);
+
+						//shooting noise
+						if(tempPlant.getCol() == tempPlant.originCol + 1) {
+							playPeaShoot();
+						}
+
+						tempPlant.column = tempPlant.column + 1;
+						if(tempPlant.getCol() == smallest) {
 							b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-							b.putPeg("peashooter", tempPlant.originRow, tempPlant.originCol);
-							b.putPeg("pea", tempPlant.getRow(),tempPlant.getCol() + 1);
-							
-							//shooting noise
-							if(tempPlant.getCol() == tempPlant.originCol + 1) {
-								playPeaShoot();
-							}
 
-							tempPlant.column = tempPlant.column + 1;
-							if(tempPlant.getCol() == smallest) {
-								b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-
-								//reset the plant animation
-								tempPlant.row = tempPlant.originRow;
-								tempPlant.column = tempPlant.originCol;
-
-								thisZomb.hp -= 2;
-								playHitZombie();
-								if(thisZomb.hp <= 0) {
-									//testing for which zombie is dying
-									System.out.println(zombies.get(index).getRow());
-									zombies.remove(index);
-
-									b.removePeg(thisZomb.getRow(), thisZomb.getCol());
-									try{Thread.sleep(1);}catch(InterruptedException e){};
-								}
-							}
-						} else {
-							b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+							//reset the plant animation
 							tempPlant.row = tempPlant.originRow;
 							tempPlant.column = tempPlant.originCol;
-						}
-					} else { //DOUBLE PEA SHOOTER
-						if(tempPlant.getCol() < 9) {
-							//animation
-							b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-							b.putPeg("doublepea", tempPlant.originRow, tempPlant.originCol);
-							b.putPeg("pea", tempPlant.getRow(),tempPlant.getCol() + 1);
-							
-							//shooting noise
-							if(tempPlant.getCol() == tempPlant.originCol + 1) {
-								playPeaShoot();
+
+							thisZomb.hp -= 4;
+							playHitZombie();
+							if(thisZomb.hp <= 0) {
+								//testing for which zombie is dying
+								System.out.println(zombies.get(index).getRow());
+								zombies.remove(index);
+
+								b.removePeg(thisZomb.getRow(), thisZomb.getCol());
 							}
-
-							tempPlant.column = tempPlant.column + 1;
-							if(tempPlant.getCol() == smallest) {
-								b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-
-								//reset the plant animation
-								tempPlant.row = tempPlant.originRow;
-								tempPlant.column = tempPlant.originCol;
-
-								thisZomb.hp -= 4;
-								playHitZombie();
-								if(thisZomb.hp <= 0) {
-									//testing for which zombie is dying
-									System.out.println(zombies.get(index).getRow());
-									zombies.remove(index);
-
-									b.removePeg(thisZomb.getRow(), thisZomb.getCol());
-								}
-							}
-						} else {
-							b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-							tempPlant.row = tempPlant.originRow;
-							tempPlant.column = tempPlant.originCol;
 						}
+					} else {
+						b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+						tempPlant.row = tempPlant.originRow;
+						tempPlant.column = tempPlant.originCol;
 					}
 				}
 			}
@@ -488,21 +507,16 @@ public class Main {
 	public static void animateSunflowers(ArrayList<Plant> plants) {
 		for(int i=0;i<plants.size();i++) {
 			Plant tempPlant = plants.get(i);
-			if(killPlant(tempPlant)) {
-				plants.remove(tempPlant);
+			b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+			if(tempPlant.plantState == 1) {
+				tempPlant.plantState = 2;
+				sunCount += 25;
+				StoredEnergy.setText("Stored Energy: " + sunCount);
+				b.putPeg("sunfloweranim", tempPlant.getRow(),tempPlant.getCol());
 			} else {
-				b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-				if(tempPlant.plantState == 1) {
-					tempPlant.plantState = 2;
-					sunCount += 25;
-					StoredEnergy.setText("Stored Energy: " + sunCount);
-					b.putPeg("sunfloweranim", tempPlant.getRow(),tempPlant.getCol());
-				} else {
-					tempPlant.plantState = 1;
-					b.putPeg("sunflower", tempPlant.getRow(),tempPlant.getCol());
-				}
-
-			}	
+				tempPlant.plantState = 1;
+				b.putPeg("sunflower", tempPlant.getRow(),tempPlant.getCol());
+			}
 		}
 	}
 
@@ -514,33 +528,16 @@ public class Main {
 	public static void animateWallnut(ArrayList<Plant> plants) {
 		for(int i=0;i<plants.size();i++) {
 			Plant tempPlant = plants.get(i);
-			if(killPlant(tempPlant)) {
-				plants.remove(tempPlant);
+			if(tempPlant.hp < 7) {
+				b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+				b.putPeg("wallnut", tempPlant.getRow(), tempPlant.getCol());
+			} else if(tempPlant.hp < 3) {
+				b.removePeg(tempPlant.getRow(), tempPlant.getCol());
+				b.putPeg("wallnut", tempPlant.getRow(), tempPlant.getCol());
 			} else {
-				if(tempPlant.hp < 7) {
-					b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-					b.putPeg("cyan", tempPlant.getRow(), tempPlant.getCol());
-				} else if(tempPlant.hp < 3) {
-					b.removePeg(tempPlant.getRow(), tempPlant.getCol());
-					b.putPeg("pink", tempPlant.getRow(), tempPlant.getCol());
-				} else {
-					b.putPeg("wallnut", tempPlant.getRow(), tempPlant.getCol());
-				}
+				b.putPeg("wallnut", tempPlant.getRow(), tempPlant.getCol());
 			}
 		}
-	}
-
-	/**
-	 * Removes plants off board
-	 * pre: plant.hp < 0
-	 * post: Removed peg
-	 */
-	public static boolean killPlant(Plant plant) { 
-		if(plant.hp == 0) {
-			b.removePeg(plant.originRow, plant.originCol);
-			return true;
-		}
-		return false;
 	}
 
 	/**
@@ -567,13 +564,27 @@ public class Main {
 			Zombie curZombie = zombies.get(i);
 			//zombie has gotten into house
 			if(curZombie.getCol() <= 0) {
-				isRunning = false;
+				gameLost = 2;
+				System.out.println("Game over!");
+				try {
+					Thread.sleep(1);
+				} catch(InterruptedException e){};
 			}
 			//plant is under attack
-			if(isRunning) {
+			if(gameLost == 0) {
 				Plant curPlant = closestPlant(curZombie);
 				if(curZombie.getCol() == curPlant.originCol + 1) {
 					curPlant.hp -= 2;
+					if(curPlant.hp == 0) {
+						b.removePeg(curPlant.originCol, curPlant.originRow);
+						if(curPlant.type == 2 || curPlant.type == 4) {
+							sunflowers.remove(index);
+						} else if(curPlant.type == 3) {
+							wallnuts.remove(index);
+						} else if(curPlant.type == 1) {
+							sunflowers.remove(index);
+						}
+					}
 					playZombieEating();
 					System.out.println("attacking plant: " + curPlant.originCol);
 				} else {
@@ -591,33 +602,37 @@ public class Main {
 	/**
 	 * Check for the plant closest to the zombie in an array
 	 * pre: Number of Plants > 0
-	 * post: Plant with the largest row value
+	 * post: Plant with the least distance value
 	 */
 	public static Plant closestPlant(Zombie curZombie) {
-		//default plant
-		Plant thisPlant = new Plant(0,0,0);
-		int largest = 0;
+		//create a "default" plant
+		Plant thisPlant = new Plant(-10,-10,0);
+		int minDistance = Integer.MAX_VALUE;
 		for(int j=0;j<shooterPlants.size();j++) {
 			Plant curPlant = shooterPlants.get(j);
-			if(curPlant.getRow() == curZombie.getRow() && curPlant.originCol > largest) {
-				largest = curPlant.originCol;
+			if(curPlant.getRow() == curZombie.getRow() && Math.abs(curZombie.getCol() - curPlant.originCol) < minDistance) {
+				minDistance = Math.abs(curZombie.getCol() - curPlant.originCol);
 				thisPlant = curPlant;
+				index = j;
 			}
 		}
 		for(int j=0;j<sunflowers.size();j++) {
 			Plant curPlant = sunflowers.get(j);
-			if(curPlant.getRow() == curZombie.getRow() && curPlant.originCol > largest) {
-				largest = curPlant.originCol;
+			if(curPlant.getRow() == curZombie.getRow() && Math.abs(curZombie.getCol() - curPlant.originCol) < minDistance) {
+				minDistance = Math.abs(curZombie.getCol() - curPlant.originCol);
 				thisPlant = curPlant;
+				index = j;
 			}
 		}
 		for(int j=0;j< wallnuts.size();j++) {
 			Plant curPlant = wallnuts.get(j);
-			if(curPlant.getRow() == curZombie.getRow() && curPlant.originCol > largest) {
-				largest = curPlant.originCol;
+			if(curPlant.getRow() == curZombie.getRow() && Math.abs(curZombie.getCol() - curPlant.originCol) < minDistance) {
+				minDistance = Math.abs(curZombie.getCol() - curPlant.originCol);
 				thisPlant = curPlant;
+				index = j;
 			}
 		}
+		System.out.println("Closest plant: " + thisPlant.getRow() + " , " + thisPlant.getCol());
 		return thisPlant;
 	}
 
@@ -658,7 +673,7 @@ public class Main {
 		}
 	}
 
-	
+
 	//Music and Sound Effects
 	public static void playPlantedNoise() {
 		try {
@@ -678,7 +693,7 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void playPeaShoot() {
 		try {
 			// Open an audio input stream.           
@@ -697,7 +712,7 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void playHitZombie() {
 		try {
 			// Open an audio input stream.           
@@ -716,7 +731,7 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void playZombieEating() {
 		try {
 			// Open an audio input stream.           
@@ -735,7 +750,7 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void playRandomGroans() {
 		if(zombies.size() > 0) {
 			Random r = new Random();
@@ -768,7 +783,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	public static void playBackgroundMusic() {
 		try {
 			// Open an audio input stream.           
